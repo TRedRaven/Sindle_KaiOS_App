0be3bef9deb5a8623ef31f3ea7c8162d8d347865
THraven <h_raven@live.nl>
2019-10-14 23:23:48 +0200

    updated the keyHandler and moved it to js/keyEventListener.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
-window.addEventListener("load", function() {
-    document.getElementsByTagName("welcomeMsg").innerHTML =
-});
+// Red - event handlers
+window.addEventListener("load", start());
+
+
+// Red - start function will run on app fully loading
+function start() {
+    // Red - the point of the app is to be text-less so this will be removed when icons are ready.
+    document.getElementsByTagName("welcomeMsg").innerHTML = "welcome Red";
+}
